a63d0f4d2242e05ce09ec6190ee4044fd56ed541
M. Zaky Pria Maulana <119280619+zkyymaulana@users.noreply.github.com>
2024-12-17 19:11:13 +0700

    fix listing ticket and history page (#32)

--- src/services/history/index.js
+++ src/services/history/index.js
@@ -1,31 +1,29 @@
-export const getHistory = async (params) => {
-  const queryParams = new URLSearchParams();
+export const getHistory = async params => {
+	const queryParams = new URLSearchParams();
 
-  if (params.startDate)
-    queryParams.append("search[startFlightDate]", params.startDate);
-  if (params.endDate)
-    queryParams.append("search[endFlightDate]", params.endDate);
-  if (params.orderId) queryParams.append("search[orderId]", params.orderId);
+	if (params.startDate) queryParams.append('search[startBookingDate]', params.startDate);
+	if (params.endDate) queryParams.append('search[endBookingDate]', params.endDate);
+	if (params.orderId) queryParams.append('search[orderId]', params.orderId);
 
-  const token = localStorage.getItem("token");
-  const url = `${import.meta.env.VITE_API_URL}${import.meta.env.VITE_API_VERSION}/orders?${queryParams.toString()}`;
+	const token = localStorage.getItem('token');
+	const url = `${import.meta.env.VITE_API_URL}${import.meta.env.VITE_API_VERSION}/orders?${queryParams.toString()}`;
 
-  try {
-    const response = await fetch(url, {
-      headers: {
-        authorization: `Bearer ${token}`,
-      },
-      method: "GET",
-    });
-    const result = await response.json();
+	try {
+		const response = await fetch(url, {
+			headers: {
+				authorization: `Bearer ${token}`,
+			},
+			method: 'GET',
+		});
+		const result = await response.json();
 
-    if (!result?.success) {
-      throw new Error(result?.message);
-    }
+		if (!result?.success) {
+			throw new Error(result?.message);
+		}
 
-    return result?.data;
-  } catch (error) {
-    console.error("Error fetching tickets:", error.message);
-    throw error;
-  }
+		return result?.data;
+	} catch (error) {
+		console.error('Error fetching tickets:', error.message);
+		throw error;
+	}
 };
