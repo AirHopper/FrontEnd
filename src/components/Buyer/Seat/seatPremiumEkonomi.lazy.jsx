5342a1ded4d9ba84896039f8481983dcf35ee100
JoF24 <ferdi24joe@gmail.com>
2024-12-23 22:22:41 +0700

    Update Name Class

--- src/components/Buyer/Seat/seatPremiumEkonomi.lazy.jsx
+++ src/components/Buyer/Seat/seatPremiumEkonomi.lazy.jsx
@@ -106,7 +106,7 @@ export default class App extends Component {
         {!!data &&(
           <>
             <Flex justifyContent="center" bg="#73CA5C" color="white" p={2} borderRadius="md" w="92%" marginLeft={5}>
-              <Text fontFamily="Inter, sans-serif">Premium Ekonomi - {availableSeats} Kursi Tersedia</Text>
+              <Text fontFamily="Inter, sans-serif">Premium Economy - {availableSeats} Kursi Tersedia</Text>
             </Flex>
             <div className="seat-picker-container">
             <SeatPicker
@@ -134,7 +134,7 @@ export default class App extends Component {
                 width="90%"
               >
                 <Text marginLeft={4} marginTop={2}>
-                  Premium Ekonomi - {selectedSeats.length} Kursi Dipilih
+                  Premium Economy - {selectedSeats.length} Kursi Dipilih
                 </Text>
                 <Box marginTop={2} marginRight={5}>
                   <FontAwesomeIcon
